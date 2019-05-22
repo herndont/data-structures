d1fde27c1799c7c20094b04f5c03bac233c00f46
herndont <herndontrey@gmail.com>
2019-05-22 13:32:52 -0700

    not completed... no whiteboard... no tests... no working code... sources for lecture looked up and added to readme

---
+++ src/main/java/MergeSort/MergeSort.java
@@ -0,0 +1,29 @@
+package MergeSort;
+
+public class MergeSort {
+
+//    taken from Baeldung, still walking through the code and trying to understand it.
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n/2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i ++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n-mid);
+
+        merge(a, l, r, mid, n -mid);
+    }
+
+    public static void merge(){
+
+    }
+}
